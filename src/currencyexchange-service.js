343c00590993ef7181403e3cadc2d8f4833d5da6
Gengur123 <meleearthur@gmail.com>
2021-07-02 10:10:11 -0700

    FIXED A BUG I DIDNT SEE FOR LIKE 30 MINUTES AHHHHHHHHHHHHHH

--- src/currencyexchange-service.js
+++ src/currencyexchange-service.js
@@ -1,8 +1,9 @@
-export class CurrencyExchanger {
-  static getNewCurrency(currency){
+export default class CurrencyExchanger {
+  static getNewCurrency(){
     return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
     .then(function(response) {
       if (!response.ok) {
+        console.log(response)
         throw Error(response.statusText);
       }
       return response.json();
