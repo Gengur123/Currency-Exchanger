b9c80e1a43ae7d05b15b76fc086487905e6b1fac
Gengur123 <meleearthur@gmail.com>
2021-07-02 11:42:25 -0700

    Added a lot of console logs to check what the api is doing

--- src/main.js
+++ src/main.js
@@ -3,18 +3,26 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import $ from 'jquery';
 import CurrencyExchanger from './currencyexchange-service';
-
+//BUSINESS LOGIC
+function displayNewCurrency(response,id, USD) {
+  console.log(response)
+  console.log(response.conversion_rates)
+  if (response.conversion_rates) {
+    console.log(response.conversion_rates[id])
+    console.log(USD)
+  } else {
+    console.log(response)
+  }
+}
+//UI LOGIC
 $(document).ready(function () {
   $('.btn-success').click(function () {
     let id = $('#currencyType').val()
-    console.log(id)
+    let USD = $('#usd').val()
+    console.log(USD)
     CurrencyExchanger.getNewCurrency()
       .then(function (response) {
-        console.log(response)
-        if (response) {
-          console.log(response)
-
-        }
-        });
-      });
+        displayNewCurrency(response, id, USD)
+      })
+    });
   });
