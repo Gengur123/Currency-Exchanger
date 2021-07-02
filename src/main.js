25eff53a9dc536bbcb9e1f25f31eee48a5b1b2f8
Gengur123 <meleearthur@gmail.com>
2021-07-02 14:03:06 -0700

    Removed console logs

--- src/main.js
+++ src/main.js
@@ -7,23 +7,20 @@ import CurrencyExchanger from './currencyexchange-service';
 //BUSINESS LOGIC
 function displayNewCurrency(response, id, USD) {
   if (!(id in response.conversion_rates)) {
-    console.log("error")
+    $('.conversion').text("error not a real currency")
   }
   else if (response.conversion_rates) {
     $('.conversion').text(`Converted Money : ${USD * response.conversion_rates[id]}` + " " + id)
-    console.log(response)
-    console.log(response.conversion_rates[id])
   } else {
-    console.log(response)
+    $('.conversion').text("error" + response)
   }
 }
+
 //UI LOGIC
 $(document).ready(function () {
-  
   $('.btn-success').click(function () {
     let id = $('#currencyType').val()
     let USD = $('#usd').val()
-    console.log(USD)
     CurrencyExchanger.getNewCurrency()
       .then(function (response) {
         displayNewCurrency(response, id, USD)
