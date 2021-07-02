f0497c41ee4a53a0f339cb00a559139885a0d544
Gengur123 <meleearthur@gmail.com>
2021-07-02 12:26:51 -0700

    Added more business logic to take in account for fake currency

--- src/main.js
+++ src/main.js
@@ -3,19 +3,23 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import $ from 'jquery';
 import CurrencyExchanger from './currencyexchange-service';
+
 //BUSINESS LOGIC
-function displayNewCurrency(response,id, USD) {
-  console.log(response)
-  console.log(response.conversion_rates)
-  if (response.conversion_rates) {
+function displayNewCurrency(response, id, USD) {
+  if (!(id in response.conversion_rates)) {
+    console.log("error")
+  }
+  else if (response.conversion_rates) {
+    $('.conversion').text(`Converted Money : ${USD * response.conversion_rates[id]}` + " " + id)
+    console.log(response)
     console.log(response.conversion_rates[id])
-    console.log(USD)
   } else {
     console.log(response)
   }
 }
 //UI LOGIC
 $(document).ready(function () {
+  
   $('.btn-success').click(function () {
     let id = $('#currencyType').val()
     let USD = $('#usd').val()
@@ -24,5 +28,5 @@ $(document).ready(function () {
       .then(function (response) {
         displayNewCurrency(response, id, USD)
       })
-    });
   });
+});
