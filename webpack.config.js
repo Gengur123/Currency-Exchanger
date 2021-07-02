9bf88a1eb62141522289d658842059802507ff43
Gengur123 <meleearthur@gmail.com>
2021-07-02 08:22:12 -0700

    Edited title in webpack and package.json

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
     new Dotenv(),
     new CleanWebpackPlugin(), // new line
     new HtmlWebpackPlugin({
-      title: 'Template',
+      title: 'CurrencyExchanger',
       template: './src/index.html',
       inject: 'body'
     })
