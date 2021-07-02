b2d26488e84355c193bf8825268f642a9f9275f7
Gengur123 <meleearthur@gmail.com>
2021-07-02 10:52:47 -0700

    Added an id to get value from user input

--- src/main.js
+++ src/main.js
@@ -2,3 +2,19 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import $ from 'jquery';
+import CurrencyExchanger from './currencyexchange-service';
+
+$(document).ready(function () {
+  $('.btn-success').click(function () {
+    let id = $('#currencyType').val()
+    console.log(id)
+    CurrencyExchanger.getNewCurrency()
+      .then(function (response) {
+        console.log(response)
+        if (response) {
+          console.log(response)
+
+        }
+        });
+      });
+  });
