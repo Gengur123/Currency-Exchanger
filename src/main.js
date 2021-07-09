8b9244f989159216595ef304b30ad4b60d849a2d
Gengur123 <meleearthur@gmail.com>
2021-07-09 09:43:51 -0700

    Reordered if else statements to better handle server errors

--- src/main.js
+++ src/main.js
@@ -6,13 +6,13 @@ import CurrencyExchanger from './currencyexchange-service';
 
 //BUSINESS LOGIC
 function displayNewCurrency(response, id, USD) {
-  if (!(id in response.conversion_rates)) {
+  console.log("here " + response.message);
+  if (response.message === "Error") {
+    $('.conversion').text(`There was a server ${response.message}`);
+  } else if (!(id in response.conversion_rates)) {
     $('.conversion').text("error not a real currency");
-  }
-  else if (response.conversion_rates) {
-    $('.conversion').text(`Converted Money : ${USD * response.conversion_rates[id]}` + " " + id);
   } else {
-    $('.conversion').text("error" + response);
+    $('.conversion').text(`Converted Money : ${USD * response.conversion_rates[id]}` + " " + id);
   }
 }
 
